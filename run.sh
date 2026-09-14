14611b00b3caaf9ebea7d08e03de1f237f733af2
asdf57 <53836417+asdf57@users.noreply.github.com>
2026-09-13 21:33:08 -0400

    Remove obsolete droplet tunnel

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-install -d -m 700 /root/.ssh
-install -m 600 /run/secrets/droplet_ssh_key /root/.ssh/id_droplet
-
-ssh -o StrictHostKeyChecking=no \
-    -f -N -R 3000:localhost:3000 \
-    -i /root/.ssh/id_droplet \
-    root@134.209.130.14
-
 exec /usr/bin/webhook -hooks /app/hooks.json -port 3000 -verbose
